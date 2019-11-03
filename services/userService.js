25a9096e29670068336a939feba2e19ee11dc74e
Gabriel Fernandes <gabriel.costa@ccc.ufcg.edu.br>
2019-11-03 14:52:18 -0300

    Add registerUser method

--- services/userService.js
+++ services/userService.js
@@ -1,31 +1,55 @@
-/**
- * Cadastra um usuario no sistema
- */
-function registerUser() {
-    let $fname = document.getElementById("register-first-name").value;
-    let $lname = document.getElementById("register-last-name").value;
-    let $email = document.getElementById("register-email").value;
-    let $credicard = document.getElementById("register-credicard").value;
-    let $password = document.getElementById("register-password").value;
+let $submit_button = document.querySelector('button');
 
-    let data = {
-        fname: $fname,
-        lname: $lname,
-        email: $email,
-        credicard: $credicard,
-        password: $password
-    };
+$submit_button.addEventListener('click', () => {
+    checkPassword();
+});
 
-    
-};
+function checkPassword() {
+    event.preventDefault();
+    let $register_password = document.querySelector('#register-password').value;
+    let $register_password_confirm = document.querySelector('#register-password-confirm').value;
 
-function confirmPassword() {
-    let $password = document.getElementById("register-password").value;
-    let $password_confirm = document.getElementById("register-password-confirm").value;
-
-    if ($password != $password_confirm) {
-        alert("As senhas devem coincidir!");
+    if ($register_password != $register_password_confirm) {
+        alert('As senhas devem coincidir.');
     } else {
         registerUser();
     }
+};
+
+function registerUser() {
+    let $fname = document.querySelector('#register-first-name').value;
+    let $lname = document.querySelector('#register-last-name').value;
+    let $email = document.querySelector('#register-email').value;
+    let $creditcard = document.querySelector('#register-creditcard').value;
+    let $password = document.querySelector('#register-password').value;
+
+    let user = {
+        fname: $fname,
+        lname: $lname,
+        email: $email,
+        creditcard: $creditcard,
+        password: $password
+    };
+
+    fetch('http://localhost:8080/v1/api/users', {
+        method: 'POST',
+        headers: {
+            'Access-Control-Allow-Origin': 'http://localhost:8000/',
+            'Content-Type': 'application/json; charset=utf-8'
+        },
+        body: JSON.stringify(user)
+    })
+    .then(response => {
+        if(!response.ok) {
+            throw new Error('Não foi possível completar o cadastro');
+        }
+        return response.text();
+    })
+    .then(() => {
+        alert('Usuário cadastrado com sucesso!');
+        window.location.href = '../index.html';
+    })
+    .catch(error => {
+        alert(error.message);
+    });
 };
