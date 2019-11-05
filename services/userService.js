bf2fc3aea8c58ed3be778dc20c8cca6100589520
Gabriel Fernandes <gabriel.costa@ccc.ufcg.edu.br>
2019-11-05 01:21:54 -0300

    Update us1

--- services/userService.js
+++ services/userService.js
@@ -1,15 +1,23 @@
 let $submit_button = document.querySelector('button');
 
 $submit_button.addEventListener('click', () => {
-    checkPassword();
+    checkFields();
 });
 
-function checkPassword() {
-    event.preventDefault();
-    let $register_password = document.querySelector('#register-password').value;
-    let $register_password_confirm = document.querySelector('#register-password-confirm').value;
+function checkFields() {
+    let $fname = document.querySelector('#register-first-name').value;
+    let $lname = document.querySelector('#register-last-name').value;
+    let $email = document.querySelector('#register-email').value;
+    let $creditcard = document.querySelector('#register-creditcard').value;
+    let $password = document.querySelector('#register-password').value;
+    let $password_confirm = document.querySelector('#register-password-confirm').value;
 
-    if ($register_password != $register_password_confirm) {
+    event.preventDefault();
+
+    if ($fname == "" || $lname == "" || $email == "" || $creditcard == "" || $password == "" || $password_confirm == "") {
+        alert('Todos os campos devem ser preenchidos!');
+    }
+    else if ($password != $password_confirm) {
         alert('As senhas devem coincidir.');
     } else {
         registerUser();
@@ -31,6 +39,20 @@ function registerUser() {
         password: $password
     };
 
+    let $loadingMsg = document.createElement('h3');
+    $loadingMsg.innerHTML = 'Aguarde um momento.';
+    $loadingMsg.style.textAlign = 'center';
+    $loadingMsg.style.color = '#4a8fda';
+    document.querySelector('#register-form-section').appendChild($loadingMsg);
+
+    let $loadingGif = document.createElement('img');
+    $loadingGif.src = '../styles/img/loading.gif'
+    $loadingGif.style.height = '50px';
+    $loadingGif.style.display = 'block';
+    $loadingGif.style.margin = '0 auto';
+    $loadingGif.style.marginTop = '10px';
+    document.querySelector('#register-form-section').appendChild($loadingGif);
+
     fetch('http://localhost:8080/v1/api/users', {
         method: 'POST',
         headers: {
@@ -41,7 +63,7 @@ function registerUser() {
     })
     .then(response => {
         if(!response.ok) {
-            throw new Error('Não foi possível completar o cadastro');
+            throw new Error('Não foi possível completar o cadastro: ' + response.status);
         }
         return response.text();
     })
