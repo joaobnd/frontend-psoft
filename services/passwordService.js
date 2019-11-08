47fce9197b62d989ef5210265587f8c24f2dedd3
Gabriel Fernandes <gabriel.costa@ccc.ufcg.edu.br>
2019-11-08 01:14:49 -0300

    Update us1a

--- services/passwordService.js
+++ services/passwordService.js
@@ -1,35 +1,37 @@
 let $submit_button = document.querySelector('#submit-btn');
 
 $submit_button.addEventListener('click', () => {
-    sendRecoveryEmail();
+    let $email = document.querySelector('#form-email').value;
+    event.preventDefault();
+
+    sendRecoveryEmail($email);
 });
 
-function sendRecoveryEmail() {
+function sendRecoveryEmail(email) {
     
-    let $email = document.querySelector('#form-email').value;
-
-    fetch('localhost:8080/v1/api/users/password/new', {
+    fetch('http://localhost:8080/v1/api/users/password/new', {
         method: 'POST',
         headers: {
             'Access-Control-Allow-Origin': '*',
             'Content-Type': 'application/json; charset=utf-8'
         },
-        body: JSON.stringify($email)
+        body: JSON.stringify(email)
     })
     .then(response => {
         if(!response.ok) {
             if (response.status == 404) {
-                alert('O e-mail inserido não está cadastrado.');
+                alert("O e-mail inserido não está cadastrado.");
+                throw new Error('Ocorreu um erro: ' + response.status);
+            } else {
+                alert('Ocorreu um erro: ' + response.status);
+                window.location.href = 'forgot-password.html';
                 throw new Error('Ocorreu um erro: ' + response.status);
             }
-            alert('Ocorreu um erro.');
-            window.location.href = 'forgot-password.html';
-            throw new Error('Ocorreu um erro: ' + response.status);
         }
         return response.text();
     })
     .then(() => {
-        alert("Em instantes você receberá um e-mail para recuperar sua senha!");
-        window.location.href = 'login.html';
-    });
+        alert('Em instantes você receberá um e-mail com um link para recuperar sua senha!');
+        window.location.href = '../index.html';
+    })
 };
