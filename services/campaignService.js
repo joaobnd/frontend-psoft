1ec6849d982f6ef8ab6a569e0d881cba949e3547
João Martins <joao.martins@ccc.ufcg.edu.br>
2019-11-08 23:21:06 -0300

    Adicionei os elementos da página de criação de campanha

--- services/campaignService.js
+++ services/campaignService.js
@@ -1,4 +1,14 @@
-if (localStorage.getItem('token') == null || localStorage.getItem('token') == '') {
-    window.location.href = "login.html";
-};
+// if (localStorage.getItem('token') == null || localStorage.getItem('token') == '') {
+//     window.location.href = "login.html";
+// };
 
+let $campaign_goal = document.querySelector('#campaign-goal');
+
+$campaign_goal.addEventListener('keyup', () => {
+    formatarMoeda();
+});
+
+function formatToCurrency() {
+    var element = document.getElementById('campaign-goal').value;
+
+}
