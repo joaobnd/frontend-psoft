7db90446684a111e17008743aa4f99b8717e2590
João <joao.martins@ccc.ufcg.edu.br>
2019-11-24 16:00:01 -0300

    Alterando a parte de perfil do usuario

--- services/profileService.js
+++ services/profileService.js
@@ -2,6 +2,49 @@ if (localStorage.getItem('token') == null || localStorage.getItem('token') == ''
     window.location.href = "login.html";
 };
 
+window.onload = () => {
+    getProfile();
+}
+
+function getProfile() {
+    let user_email = localStorage.getItem('email');
+
+    fetch('https://api-ajudepsoft.herokuapp.com/v1/api/users/' + user_email, {
+        method: 'GET',
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'application/json; charset=utf-8'
+        }
+    })
+    .then(response => {
+        if(response.status == 404) {
+            window.location.href = 'campaigns.html';
+            throw new Error('not found')
+        } else if(response.status == 500) {
+            window.location.href = 'index.html';
+            logout();
+            alert('Faça login novamente!');
+        }
+        response.json().then(data => {
+            renderProfile(data);
+            console.log(data);
+        });
+    })
+    .catch(error => {
+        alert(error.message);
+    });
+}
+
+function renderProfile(user) {
+    let $profile_fname = document.querySelector('#profile-fname');
+    let $profile_lname = document.querySelector('#profile-lname');
+    let $profile_email = document.querySelector('#profile-email');
+
+    $profile_fname.value = user.firstName;
+    $profile_lname.value = user.lastName;
+    $profile_email.value = user.email;
+}
+
 let $logoutButton = document.querySelector("#logout-btn");
 
 $logoutButton.addEventListener('click', () => {
